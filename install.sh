b37df8d0130dd364eea36d2c9c37f6e880a4cc8a
Jérémy WALTHER <jeremy.walther@golflima.net>
2018-06-05 22:30:15 +0200

    Change RPi SSH port to 2022

--- install.sh
+++ install.sh
@@ -15,5 +15,10 @@ sudo apt-get install -y docker-compose
 git clone https://github.com/virtua-sa/gitci314.git 
 cd gitci314
 
+# Change Raspberry Pi SSH port from 22 to 2022
+# (to be able to use Git over SSH on default port)
+sed -i 's/^Port .*/Port 2022/g' /etc/ssh/sshd_config
+sudo /etc/init.d/ssh restart
+
 # Run Gitci314 !
 screen docker-compose up -d
