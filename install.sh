4da6d5a952db7cc4c6ceec9a8bfd9d0728982155
Jérémy WALTHER <jeremy.walther@golflima.net>
2018-06-06 14:13:16 +0200

    Remove swap from docker

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get install -y \
     git \
+    pv \
     screen
 
 # Install Docker
@@ -14,6 +15,24 @@ sudo apt-get install -y docker-compose
 # Clone Gitci314 repository
 git clone https://github.com/virtua-sa/gitci314.git 
 cd gitci314
+
+# Increase swap size to 4GB
+# SWAP may reduce dramatically the lifespan of your RPi SD Card
+# So we highly recommend to move following file on a USB drive
+# Path of the swap file:
+${SWAPFILE}=${SWAPFILE:-/var/swap}
+# Size of the swap file, in GiB:
+${SWAPSIZE}=${SWAPSIZE:-4}
+sudo dphys-swapfile swapoff
+free -m
+sudo sed -i "s/CONF_SWAPSIZE/CONF_SWAPSIZE=$((SWAPSIZE*1024))/g" /etc/dphys-swapfile
+sudo sed -i "s/CONF_SWAPFILE/CONF_SWAPFILE=${SWAPFILE}/g" /etc/dphys-swapfile
+sudo dd if=/dev/zero | pv -s ${SWAPSIZE}G | sudo dd of=${SWAPFILE} bs=1024 count=$((SWAPSIZE*1024*1024))
+sudo chown root:root ${SWAPFILE}
+sudo chmod 0600 ${SWAPFILE}
+sudo mkswap ${SWAPFILE}
+sudo dphis-swapfile swapon
+free -m
 
 # Change Raspberry Pi SSH port from 22 to 2022
 # (to be able to use Git over SSH on default port)
